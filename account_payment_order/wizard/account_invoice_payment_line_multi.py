9dade634373eb794b0c0d2a6ece2ad44519a6ba6
qgigon <gigon.quentin@gmail.com>
2020-06-02 15:49:09 +0200

    setting the local_instrument of the lines when adding the invoice to a payment/debit order

--- account_payment_order/wizard/account_invoice_payment_line_multi.py
+++ account_payment_order/wizard/account_invoice_payment_line_multi.py
@@ -16,4 +16,14 @@ class AccountInvoicePaymentLineMulti(models.TransientModel):
         invoices = self.env['account.invoice'].browse(
             self._context['active_ids'])
         action = invoices.create_account_payment_line()
+
+        order = self.env[action['res_model']].browse(action['res_id'])
+        local_instrument = ""
+        if order.company_partner_bank_id.acc_type == "iban":
+            local_instrument = "LSV+"
+        elif order.company_partner_bank_id.acc_type == "postal":
+            local_instrument = "DDCOR1"
+        order.payment_line_ids.write({
+            "local_instrument": local_instrument
+        })
         return action
